1a8b31f050cedb13b9801e1f5aca8905e7adf59c
Amit Alon <amit@kaholo.io>
2021-06-27 17:58:33 +0300

    refactored to new format

--- src/app.js
+++ src/app.js
@@ -1,36 +1,24 @@
-const config = require("./config");
-const mapExecutionService = require("../../../api/services/map-execution.service");
-const Trigger = require("../../../api/models/map-trigger.model");
+async function webhook(req, res, settings, triggerControllers) {
+    if (!triggerControllers) {
+        return res.status(400).send("triggers cannot be nil");
+    }
+    try {        
+        const body = req.body;
+        const reqSecret = body.secret, reqSeverity = body.severity;
+        triggerControllers.forEach(trigger => {
+            const secret = trigger.params.SECRET, severity = trigger.params.SEVERITY;
+            if (secret && reqSecret !== secret) return;
+            if (severity && reqSeverity != severity) return;
+            const msg = `${trigger.name} Alert Severity ${reqSeverity}`;
+            trigger.execute(msg, body);
+        });
+        res.status(200).send("OK");
+    }
+    catch (err){
+        res.status(422).send(err.message);
+    }
+}
 
 module.exports = {
-    webhook: function (req, res) {
-        let p = new Promise((resolve, reject) => {
-            resolve(Trigger.find({ plugin: config.name }))
-        }).then((triggers) => {
-            console.log("Found trigger", triggers.length);
-            triggers.forEach(trigger =>
-                new Promise((resolve, reject) => {
-                    
-                    const triggerServerity = trigger.params.find(o => o.name === 'SEVERITY');
-                    const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
-                    
-                    if(req.body.secret != triggerSecret.value)
-                        return reject("Invalid Secret");
-                    
-                    if(triggerServerity.value != req.body.severity) 
-                        return reject("Severity not match");
-                    
-                    resolve();
-                }).then(() => {
-                    console.log(trigger.map);
-                    let message = trigger.name + ' - Started by zabbix trigger';
-                    mapExecutionService.execute(trigger.map, null, 0, req, trigger.configuration, message);
-                })
-            )
-        })
-        .then(() =>{
-            res.send('OK');
-        })
-        .catch((error) => res.send(error))
-    }
-};
+    webhook
+}
